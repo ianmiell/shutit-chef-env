5c471960ef43a2f5de94e1de08e5b61dc5ad4308
Ian Miell <ian.miell@gmail.com>
2017-10-24 15:44:19 +0100

    latest

--- shutit_chef_env.py
+++ shutit_chef_env.py
@@ -12,7 +12,7 @@ class shutit_chef_env(ShutItModule):
 		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
 		gui = shutit.cfg[self.module_id]['gui']
 		memory = shutit.cfg[self.module_id]['memory']
-		run_dir = '/space/git/shutit-chef-env/vagrant_run'   
+		run_dir = '/space/git/shutit-chef-env/vagrant_run'
 		module_name = 'shutit_chef_env_' + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
 		shutit.send('command rm -rf ' + run_dir + '/' + module_name + ' && command mkdir -p ' + run_dir + '/' + module_name + ' && command cd ' + run_dir + '/' + module_name)
 		if shutit.send_and_get_output('vagrant plugin list | grep landrush') == '':
@@ -25,11 +25,11 @@ class shutit_chef_env(ShutItModule):
     vb.memory = "''' + memory + '''"
   end
 
-  config.vm.define "chefserver" do |chefserver|    
+  config.vm.define "chefserver" do |chefserver|
     chefserver.vm.box = ''' + '"' + vagrant_image + '"' + '''
     chefserver.vm.hostname = "chefserver.vagrant.test"
     chefserver.vm.provider :virtualbox do |v|
-      v.customize ["modifyvm", :id, "--memory", "1024"]
+      v.customize ["modifyvm", :id, "--memory", "2048"]
     end
   end
   config.vm.define "chefworkstation1" do |chefworkstation1|
@@ -47,7 +47,10 @@ class shutit_chef_env(ShutItModule):
     end
   end
 end''')
-		pw = shutit.get_env_pass()                                                                                                                                                
+		try:
+			pw = open('secret').read().strip()
+		except:
+			pw = shutit.get_env_pass()
 		try:
 			shutit.multisend('vagrant up --provider ' + shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'],{'assword for':pw},timeout=99999)
 		except:
@@ -58,11 +61,12 @@ end''')
 		for machine in machine_names:
 			shutit.login(command='vagrant ssh ' + machine)
 			shutit.login(command='sudo su -',password='vagrant')
+			shutit.send('wget -qO- https://raw.githubusercontent.com/ianmiell/vagrant-swapfile/master/vagrant-swapfile.sh | sh')
 			shutit.send('''sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config''')
 			shutit.send('''sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config''')
 			shutit.send('service sshd restart')
 			shutit.send('echo root:' + root_pass + ' | /usr/sbin/chpasswd')
-			shutit.multisend('ssh-keygen',{'Enter file':'','Enter passphrase':'','Enter same pass':''}) 
+			shutit.multisend('ssh-keygen',{'Enter file':'','Enter passphrase':'','Enter same pass':''})
 			shutit.logout()
 			shutit.logout()
 		for machine in machine_names:
@@ -77,10 +81,11 @@ end''')
 		shutit.login(command='vagrant ssh chefserver')
 		shutit.login(command='sudo su -',password='vagrant')
 		shutit.send('hostname -f',note='Check the hostname is meaningful')
-		shutit.send('wget https://github.com/ianmiell/shutit-chef-env/raw/master/chef-server-core_12.16.14-1_amd64.deb.xaa')
-		shutit.send('wget https://github.com/ianmiell/shutit-chef-env/raw/master/chef-server-core_12.16.14-1_amd64.deb.xab')
+		shutit.send('wget -q https://github.com/ianmiell/shutit-chef-env/raw/master/chef-server-core_12.16.14-1_amd64.deb.xaa')
+		shutit.send('wget -q https://github.com/ianmiell/shutit-chef-env/raw/master/chef-server-core_12.16.14-1_amd64.deb.xab')
 		shutit.send('cat chef-server-core_12.16.14-1_amd64.deb.xaa chef-server-core_12.16.14-1_amd64.deb.xab > chef-server-core_12.16.14-1_amd64.deb && rm *xaa *xab')
 		shutit.send('dpkg -i chef-server-core_*.deb',note='Install the package')
+		shutit.send(r'''sed -i 's/^\(127.0.0.1[ \t]*[^ \t]*\).*/\1/' /etc/hosts''')
 		shutit.send('chef-server-ctl reconfigure',note='Set up the chef server on this host')
 		shutit.send('chef-server-ctl install chef-manage',note='Install chef manager')
 		shutit.send("""chef-server-ctl user-create admin admin admin admin@example.com examplepass -f admin.pem""",note='Create the admin user certificate')
@@ -93,8 +98,9 @@ end''')
 
 		shutit.login(command='vagrant ssh chefworkstation1')
 		shutit.login(command='sudo su -',password='vagrant')
+		shutit.send(r'''sed -i 's/^\(127.0.0.1[ \t]*[^ \t]*\).*/\1/' /etc/hosts''')
 		shutit.send('cd /root')
-		shutit.send('wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_1.0.3-1_amd64.deb')
+		shutit.send('wget -q https://packages.chef.io/stable/ubuntu/12.04/chefdk_1.0.3-1_amd64.deb')
 		shutit.send('dpkg -i chefdk*deb')
 		shutit.install('git')
 		shutit.send('git config --global user.name "Your Name"')
@@ -131,7 +137,8 @@ cookbook_path            ["#{current_dir}/../cookbooks"]'''
 
 		shutit.login(command='vagrant ssh chefnode1')
 		shutit.login(command='sudo su -',password='vagrant')
-		shutit.send('wget https://packages.chef.io/stable/ubuntu/12.04/chef_12.16.42-1_amd64.deb')
+		shutit.send(r'''sed -i 's/^\(127.0.0.1[ \t]*[^ \t]*\).*/\1/' /etc/hosts''')
+		shutit.send('wget -q https://packages.chef.io/stable/ubuntu/12.04/chef_12.16.42-1_amd64.deb')
 		shutit.send('dpkg -i chef_12.16.42-1_amd64.deb ')
 		shutit.send('mkdir .chef')
 		shutit.send('cd .chef')
@@ -139,11 +146,10 @@ cookbook_path            ["#{current_dir}/../cookbooks"]'''
 		shutit.send_file('/root/.chef/admin.pem',admin_pem)
 		shutit.send('knife ssl fetch')
 		shutit.send('knife bootstrap -N chefnode1 chefnode1')
-		shutit.pause_point('node added?')
 		shutit.logout()
 		shutit.logout()
 
-		shutit.login(command='vagrant ssh workstation1')
+		shutit.login(command='vagrant ssh chefworkstation1')
 		shutit.login(command='sudo su -',password='vagrant')
 		shutit.install('epel-release')
 		shutit.install('git')
@@ -173,23 +179,23 @@ cookbook_path            ["#{current_dir}/../cookbooks"]''')
 		shutit.logout()
 		shutit.logout()
 
-		for machine in ('node1','node2'):
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su -',password='vagrant')
-			shutit.install('epel-release')
-			shutit.send('rpm -i https://packages.chef.io/stable/el/7/chefdk-0.19.6-1.el7.x86_64.rpm')
-			shutit.install('git')
-			shutit.send('mkdir -p /etc/chef')
-			shutit.send_file('/etc/chef/client.rb',"""#/etc/chef/client.rb on Node
-log_level        :info
-log_location     STDOUT
-chef_server_url  'http://master1.local:4000'
-validation_client_name 'kramerica-validator'""")
-			shutit.send_file('/etc/chef/kramerica-validator.pem',kramer_pem)
-			shutit.pause_point('set up node1 ok?')
-			shutit.logout()
-			shutit.logout()
-		shutit.pause_point('set up node1')
+		#for machine in ('node1','node2'):
+		#	shutit.login(command='vagrant ssh ' + machine)
+		#	shutit.login(command='sudo su -',password='vagrant')
+		#	shutit.install('epel-release')
+		#	shutit.send('rpm -i https://packages.chef.io/stable/el/7/chefdk-0.19.6-1.el7.x86_64.rpm')
+		#	shutit.install('git')
+		#	shutit.send('mkdir -p /etc/chef')
+		#	shutit.send_file('/etc/chef/client.rb',"""#/etc/chef/client.rb on Node
+#log_level        :info
+#log_location     STDOUT
+#chef_server_url  'http://master1.local:4000'
+#validation_client_name 'kramerica-validator'""")
+		#	shutit.send_file('/etc/chef/kramerica-validator.pem',kramer_pem)
+		#	shutit.pause_point('set up node1 ok?')
+		#	shutit.logout()
+		#	shutit.logout()
+		shutit.pause_point('all done?')
 		return True
 
 	def get_config(self, shutit):
@@ -201,7 +207,7 @@ validation_client_name 'kramerica-validator'""")
 
 def module():
 	return shutit_chef_env(
-		'cookbook-openshift3.shutit_chef_env.shutit_chef_env', 1355141237.0001,   
+		'cookbook-openshift3.shutit_chef_env.shutit_chef_env', 1355141237.0001,
 		description='',
 		maintainer='',
 		delivery_methods=['bash'],
